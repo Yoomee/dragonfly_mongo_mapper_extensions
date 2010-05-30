8b09e7934a5486abd8b964692ae6903165c5d7b4
Tomas Celizna <tomascelizna@Tomas-Celizna-MacBook-Pro.local>
2010-05-30 11:35:36 +0200

    uid is saved as a string

--- lib/dragonfly/data_storage/mongo_grid_fs_store.rb
+++ lib/dragonfly/data_storage/mongo_grid_fs_store.rb
@@ -9,20 +9,19 @@ module Dragonfly
       end
       
       def store(temp_object)
-        # returns object id
-        id = @grid.put(File.read(temp_object.path), :filename => temp_object.basename)
-        id
+        uid = @grid.put( File.read(temp_object.path), :filename => temp_object.basename )
+        uid.to_s
       end
 
       def retrieve(uid)
-        file = @grid.get(BSON::ObjectID.from_string(uid))
+        file = @grid.get( BSON::ObjectID.from_string(uid) )
         file.read
       rescue
         raise DataNotFound, $!.message  
       end
       
       def destroy(uid)
-        @grid.delete(BSON::ObjectID.from_string(uid))
+        @grid.delete( BSON::ObjectID.from_string(uid) )
       rescue
         raise DataNotFound, $!.message
       end
